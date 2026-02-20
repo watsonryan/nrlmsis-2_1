2725013273a79b9f6263c28a7bb8b674419b8626
watsonryan <rwatso12@gmail.com>
2026-02-19 20:03:23 -0700

    bootstrap project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# NRLMSIS 2.1 C++20 Port
+
+Work-in-progress C++20 port of NRLMSIS 2.1 Fortran reference implementation.
---
+++ src/msis21.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file msis21.cpp
+ * @brief Library translation unit anchor.
+ * @author Watosn
+ */
+
+namespace msis21 {
+
+int msis21_anchor() { return 0; }
+
+}  // namespace msis21
